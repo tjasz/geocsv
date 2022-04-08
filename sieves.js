32502a2a441f599668f15031849b1645ec7fb605
tjasz <nukorausa@hotmail.com>
2022-04-08 14:52:25 -0700

    draft sieve model for filtering

---
+++ sieves.js
@@ -0,0 +1,38 @@
+function NumberSieve() {
+}
+NumberSieve.prototype.setMin = function(min) {
+  this.min = min;
+};
+NumberSieve.prototype.setMax = function(max) {
+  this.max = max;
+};
+NumberSieve.prototype.setRange = function(min, max) {
+  this.min = min;
+  this.max = max;
+};
+NumberSieve.prototype.predicate = function(x) {
+  result = true;
+  if (this.min) result = result && this.min < x;
+  if (this.max) result = result && this.max > x;
+  return result;
+};
+
+function FieldSieve() {
+  this.sieves = {}
+}
+FieldSieve.prototype.clear = function() {
+  this.sieves = {}
+};
+FieldSieve.prototype.clear = function(field) {
+  delete this.sieves[field];
+};
+FieldSieve.prototype.set = function(field, sieve) {
+  this.sieves[field] = sieve;
+};
+FieldSieve.prototype.predicate = function(item) {
+  result = true;
+  for (const [field, sieve] of Object.entries(this.sieves)) {
+    result = result && sieve.predicate(item[field]);
+  }
+  return result;
+};
